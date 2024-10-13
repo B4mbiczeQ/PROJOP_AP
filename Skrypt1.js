1254d6d6eb2bcf6229a93e11886e8bfb372155cc
CoolGuy <maksglinicki09@gmail.com>
2024-10-13 16:58:28 +0200

    Second commit

---
+++ Skrypt1.js
@@ -0,0 +1,20 @@
+function Calculate(){
+    var Liczba1 = parseFloat(document.getElementById('liczba1').value)
+    var Liczba2 = parseFloat(document.getElementById('liczba2').value)
+    var Wynik1 = document.getElementById('opcje').value
+
+    switch(Wynik1){
+        case "+":
+            var Liczba2 = document.getElementById('Wynik').value = Liczba1 + " + " + Liczba2 + " = " + (Liczba1 + Liczba2)
+            break;
+        case "-":
+            var Liczba2 = document.getElementById('Wynik').value = Liczba1 + " - " + Liczba2 + " = " + (Liczba1 - Liczba2)
+            break;
+        case "*":
+            var Liczba2 = document.getElementById('Wynik').value = Liczba1 + " * " + Liczba2 + " = " + (Liczba1 * Liczba2)
+            break;
+        case "/":
+            var Liczba2 = document.getElementById('Wynik').value = Liczba1 + " / " + Liczba2 + " = " + (Liczba1 / Liczba2)
+            break;
+    }
+}
