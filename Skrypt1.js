6d41f92bbfb86d2206058134d5fbc8eeded8d924
CoolGuy <maksglinicki09@gmail.com>
2024-10-18 17:49:51 +0200

    third commit

--- Skrypt1.js
+++ Skrypt1.js
@@ -1,19 +1,34 @@
-function Calculate(){
-var Liczba1=parseFloat(document.getElementById('liczba1').value)
-var Liczba2=parseFloat(document.getElementById('liczba2').value)
-var Wynik1 = document.getElementById('opcje').value
-switch(Wynik1){
-case "+":
-var Liczba2=document.getElementById('Wynik').value = Liczba1 + " + " + Liczba2 + " = " + (Liczba1 + Liczba2)
-break;
-case "-":
-var Liczba2=document.getElementById('Wynik').value = Liczba1 + " - " + Liczba2 + " = " + (Liczba1 - Liczba2)
-break;
-case "*":
-var Liczba2=document.getElementById('Wynik').value = Liczba1 + " * " + Liczba2 + " = " + (Liczba1 * Liczba2)
-break;
-case "/":
-var Liczba2=document.getElementById('Wynik').value = Liczba1 + " / " + Liczba2 + " = " + (Liczba1 / Liczba2)
-break;
-}
-}
+    function Calculate(){
+
+        var Liczba_Pierwsza = parseFloat(document.getElementById('PierwszaLiczba').value); //Użytkownik podaje Pierwszą liczbę które będzie po przecinku.
+        var Liczba_Druga = parseFloat(document.getElementById('DrugaLiczba').value);
+        var Opcja_wyboru = document.getElementById('Wybór').value; // Użytkownik podaje operację jaką chciałby dokonać na 2 liczbach przez niego wybranych.
+        
+        
+
+        PodajWynik(Liczba_Pierwsza,Liczba_Druga,Opcja_wyboru); // Funkcja, dzięki której użytkownik zobaczy operację na jego dwóch wybranych liczbach i wybranej przez niego operacji arytmetycznej (Dodawania/Odejmowania/Mnożenie/Dzielenia).
+    }
+    
+    function PodajWynik(ValueFirst,ValueSecond,Operator){
+
+        var Result; // Tworzymy zmienną, która później wyświetli wynik pomiędzy 2 liczbami wybranymi przez użytkownika.
+        
+
+        switch(Operator){
+            case "+":
+                Result = (ValueFirst + ValueSecond); // Za pomocą "switch" ustawiemy zmiennej wartość, która obliczy 2 liczby wybrane przez użytkownika.
+                break;
+            case "-":
+                Result = (ValueFirst - ValueSecond);
+                break;
+            case "*":
+                Result = (ValueFirst * ValueSecond);
+                break;
+            case "/":
+                Result = (ValueFirst / ValueSecond);
+                break;
+        }       
+
+        ValueSecond = document.getElementById('Wynik').value = ValueFirst + ` ${Operator} ` + ValueSecond + " = " + (Result); // Końcowo to będzie przykładowy wynik operacji na 2 liczbach przez użytkownika: 2 + 2 = 4 lub 7 / 7 = 1. 
+
+    } 
