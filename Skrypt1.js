b44a02f14ef70a9f7b6c06a9694f782195de13be
CoolGuy <maksglinicki09@gmail.com>
2025-03-06 19:25:09 +0100

    fifth commit

--- Skrypt1.js
+++ Skrypt1.js
@@ -1,34 +1,54 @@
-    function Calculate(){
+    let operation = ""
 
-        var Liczba_Pierwsza = parseFloat(document.getElementById('PierwszaLiczba').value); //Użytkownik podaje Pierwszą liczbę które będzie po przecinku.
-        var Liczba_Druga = parseFloat(document.getElementById('DrugaLiczba').value);
-        var Opcja_wyboru = document.getElementById('Wybór').value; // Użytkownik podaje operację jaką chciałby dokonać na 2 liczbach przez niego wybranych.
-        
-        
+    function numberclick(number){
 
-        PodajWynik(Liczba_Pierwsza,Liczba_Druga,Opcja_wyboru); // Funkcja, dzięki której użytkownik zobaczy operację na jego dwóch wybranych liczbach i wybranej przez niego operacji arytmetycznej (Dodawania/Odejmowania/Mnożenie/Dzielenia).
+        if( operation.length >= 17 ){}
+        else{
+
+            operation += number
+            updateResult(operation)
+
+        } 
+
     }
-    
-    function PodajWynik(ValueFirst,ValueSecond,Operator){
 
-        var Result; // Tworzymy zmienną, która później wyświetli wynik pomiędzy 2 liczbami wybranymi przez użytkownika.
+    function opclick(operator){
+
+        if( operation.length >= 17 ){}
+        else{
+
+            if(operation.lastIndexOf != operator){
+
+                operation += operator
+                updateResult(operation)
+
+            }
+
+        }
         
+    }
 
-        switch(Operator){
-            case "+":
-                Result = (ValueFirst + ValueSecond); // Za pomocą "switch" ustawiemy zmiennej wartość, która obliczy 2 liczby wybrane przez użytkownika.
-                break;
-            case "-":
-                Result = (ValueFirst - ValueSecond);
-                break;
-            case "*":
-                Result = (ValueFirst * ValueSecond);
-                break;
-            case "/":
-                Result = (ValueFirst / ValueSecond);
-                break;
-        }       
+    function clearCalc(){
 
-        ValueSecond = document.getElementById('Wynik').value = ValueFirst + ` ${Operator} ` + ValueSecond + " = " + (Result); // Końcowo to będzie przykładowy wynik operacji na 2 liczbach przez użytkownika: 2 + 2 = 4 lub 7 / 7 = 1. 
+        operation = ""
+        updateResult(operation)
 
-    } 
+    }
+
+    function updateResult(Item){
+        
+        textbox = document.getElementById("Item_cont")
+        textbox.value = Item
+
+    }
+
+    function showAnswer(){
+
+        if(operation.length > 0){
+
+            operation = eval(operation)
+            updateResult(operation)
+
+        }
+        
+    }
